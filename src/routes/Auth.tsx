146ed8f593695363e8d5fb0f8ab88090d228aebe
soultree-fly <yuyaebean@gmail.com>
2021-01-03 00:18:17 +0900

    2.1 Login Form part 1

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,5 +1,52 @@
+import { useState } from 'react';
+
 function Auth() {
-  return <span>Auth</span>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const {
+      target: { name, value },
+    } = event;
+
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  }
+
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button type="button">Continue with Google</button>
+        <button type="button">Continue with GitHub</button>
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
