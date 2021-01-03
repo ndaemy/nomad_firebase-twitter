0e999ab9d8b8348f02e8d0cb316e9a0e159ef947
soultree-fly <yuyaebean@gmail.com>
2021-01-03 13:14:06 +0900

    2.2 Login Form part 2

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -24,7 +24,7 @@ function Auth() {
     <div>
       <form onSubmit={onSubmit}>
         <input
-          type="text"
+          type="email"
           placeholder="Email"
           name="email"
           required
