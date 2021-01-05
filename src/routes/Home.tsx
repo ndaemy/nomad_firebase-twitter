0d93b6a7c27891adb0942fd364f9f8ae789aa646
soultree-fly <yuyaebean@gmail.com>
2021-01-05 19:48:35 +0900

    3.0 Form and Databasse Setup

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+
 function Home() {
-  return <span>Home</span>;
+  const [tweet, setTweet] = useState('');
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const {
+      target: { value },
+    } = e;
+    setTweet(value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder="What's on your mind?"
+          maxLength={120}
+          value={tweet}
+          onChange={handleChange}
+        />
+        <input type='submit' value='Nweet' />
+      </form>
+    </div>
+  );
 }
 
 export default Home;
